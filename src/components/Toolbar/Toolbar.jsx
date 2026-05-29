73ebbe082a9757b239376b3e20ee90bda2974a08
fabifabian <fabianharizi123@gmail.com>
2026-05-29 17:44:32 +0200

    Extracted toolset in a util export

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -1,19 +1,8 @@
-import { Circle, Hand, MousePointer2, Pointer, RectangleHorizontal, Square } from 'lucide-react'
 import styles from './Toolbar.module.css'
-import { act, useEffect } from 'react';
+import { useEffect } from 'react';
+import toolset from '../../utils/toolset.js'
 
 export default function Toolbar({activeTool, setActiveTool}){
-  const tools = [
-    [
-      {id: "select", icon: MousePointer2},
-      {id: "move", icon: Hand}
-    ],
-    [
-      {id: "rectangle", icon: Square},
-      {id: "oval", icon: Circle}
-    ]
-  ]
-  
     useEffect(() => {
       const handleKeyDown = (e) => {
         if(e.key == " "){
@@ -35,7 +24,7 @@ export default function Toolbar({activeTool, setActiveTool}){
 
   return (
     <div className={styles.toolbar}>
-      {tools.map((group, index) => <div key={"tb-"+index} className={styles.group}>
+      {toolset.map((group, index) => <div key={"tb-"+index} className={styles.group}>
       {group.map((tool) => <button key={"tb-"+tool.id} className={(tool.id === activeTool) ? styles.active : ""} onClick={() => setActiveTool(tool.id)}><tool.icon /></button>)}
       </div>)}
     </div>
---
+++ src/utils/toolset.js
@@ -0,0 +1,14 @@
+import { Circle, Hand, MousePointer2, Square } from "lucide-react";
+
+const toolset = [
+  [
+    { id: "select", icon: MousePointer2 },
+    { id: "move", icon: Hand }
+  ],
+  [
+    { id: "rectangle", icon: Square },
+    { id: "oval", icon: Circle }
+  ]
+]
+
+export default toolset
